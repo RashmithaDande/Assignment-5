e9d150e95fed4d4e346847e939aba2c54d5c0c87
RashmithaDande <132337319+RashmithaDande@users.noreply.github.com>
2023-12-13 19:40:21 +0530

    Add files via upload

---
+++ SMS_API_SpringBoot_Backend/src/main/java/in/co/vwits/sms/model/User.java
@@ -0,0 +1,37 @@
+package in.co.vwits.sms.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_user")
+public class User {
+
+	@Id
+	private String username;
+	@Column(nullable=false)
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + "]";
+	}
+	
+	
+	
+	
+}
---
+++ SMS_API_SpringBoot_Backend/src/main/java/in/co/vwits/sms/service/UserService.java
@@ -0,0 +1,19 @@
+package in.co.vwits.sms.service;
+
+import java.util.List;
+
+import in.co.vwits.sms.model.User;
+
+public interface UserService {
+
+    void create(User s);
+	
+	List<User> findAll();
+	
+	User findOneUser(String username);
+	
+	void delete(String username);
+	
+	User updateUser(User user);
+	 
+}
